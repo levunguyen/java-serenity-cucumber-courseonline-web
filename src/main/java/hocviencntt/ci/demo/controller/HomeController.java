5d63c9f9aa21574f7763334486d2d3b8b15fbcd5
framgia <framgia@framgia-Inspiron-3459>
2016-11-23 14:33:43 +0700

    mvc

---
+++ src/main/java/hocviencntt/ci/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package hocviencntt.ci.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView setupForm() {
+		System.out.println("HELLO");
+		ModelAndView model = new ModelAndView("home");
+		return model;
+
+	}
+
+}
